9fee1e863a5e5d3959f6b69e8184d9f5caee23ab
Wing Huang <huangsen365@gmail.com>
2017-11-05 17:38:56 +0800

    adding 5*-c7-systemd-*-php70-fpm-useradd-sshuser

---
+++ 5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/mkdir.sh
@@ -0,0 +1,7 @@
+mkdir -p /tmp/default_paths_for_docker/etc/httpd
+mkdir -p /tmp/default_paths_for_docker/etc/nginx
+mkdir -p /tmp/default_paths_for_docker/etc/opt/rh/rh-php70
+mkdir -p /tmp/default_paths_for_docker/etc/php-fpm.d
+mkdir -p /tmp/default_paths_for_docker/home
+mkdir -p /tmp/default_paths_for_docker/root
+mkdir -p /tmp/default_paths_for_docker/var/log
---
+++ 5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/rsync.sh
@@ -0,0 +1,7 @@
+rsync -av /etc/httpd/ /tmp/default_paths_for_docker/etc/httpd/
+rsync -av /etc/nginx/ /tmp/default_paths_for_docker/etc/nginx/
+rsync -av /etc/opt/rh/rh-php70/ /tmp/default_paths_for_docker/etc/opt/rh/rh-php70/
+rsync -av /etc/php-fpm.d/ /tmp/default_paths_for_docker/etc/php-fpm.d/
+rsync -av /home/ /tmp/default_paths_for_docker/home/
+rsync -av /root/ /tmp/default_paths_for_docker/root/
+rsync -av /var/log/ /tmp/default_paths_for_docker/var/log/
