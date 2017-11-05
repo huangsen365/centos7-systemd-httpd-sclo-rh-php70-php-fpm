7a0e00ae276a13d8dac152bd1bd303fa92b6a733
Wing Huang <huangsen365@gmail.com>
2017-11-05 18:52:18 +0800

    modified:   5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir.sh 	modified:   5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/rsync.sh 	modified:   5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/mkdir.sh 	modified:   5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/rsync.sh

--- 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/rsync.sh
+++ 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/rsync.sh
@@ -5,3 +5,4 @@ rsync -av /etc/php-fpm.d/ /tmp/default_paths_for_docker/etc/php-fpm.d/
 rsync -av /home/ /tmp/default_paths_for_docker/home/
 rsync -av /root/ /tmp/default_paths_for_docker/root/
 rsync -av /var/log/ /tmp/default_paths_for_docker/var/log/
+rsync -av /var/www/ /tmp/default_paths_for_docker/var/www/
--- 5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/mkdir.sh
+++ 5b-c7-systemd-nginx-php70-fpm-useradd-sshuser/mkdir.sh
@@ -5,3 +5,4 @@ mkdir -p /tmp/default_paths_for_docker/etc/php-fpm.d
 mkdir -p /tmp/default_paths_for_docker/home
 mkdir -p /tmp/default_paths_for_docker/root
 mkdir -p /tmp/default_paths_for_docker/var/log
+mkdir -p /tmp/default_paths_for_docker/var/www
