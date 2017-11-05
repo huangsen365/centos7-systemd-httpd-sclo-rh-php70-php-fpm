c9a2798a12f737a0705509e1517c14d874f76ef2
Wing Huang <huangsen365@gmail.com>
2017-11-05 23:44:23 +0800

    modified:   Dockerfile 	renamed:    chown_chmod.sh -> mkdir_chown_chmod.sh

--- 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir_chown_chmod.sh
+++ 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir_chown_chmod.sh
@@ -1,3 +1,4 @@
+#mkdir /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com; mkdir /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com; mkdir /var/www/www.yourdomain.com;
 chown -R sshuser:sshuser /home/sshuser
 chown -R root:apache /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
 chmod 770 /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
