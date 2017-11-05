834ff92b0e3a225033bf907ff7d4c7bfe22a5dea
Wing Huang <huangsen365@gmail.com>
2017-11-05 23:48:09 +0800

    modified:   ../5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir_chown_chmod.sh

--- 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir_chown_chmod.sh
+++ 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/mkdir_chown_chmod.sh
@@ -1,7 +1,14 @@
 #mkdir /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com; mkdir /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com; mkdir /var/www/www.yourdomain.com;
+
 chown -R sshuser:sshuser /home/sshuser
+
+mkdir /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
 chown -R root:apache /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
 chmod 770 /var/opt/rh/rh-php70/lib/php/session/www.yourdomain.com
+
+mkdir /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
 chown -R root:apache /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
 chmod 770 /var/opt/rh/rh-php70/lib/php/wsdlcache/www.yourdomain.com
+
+mkdir /var/www/www.yourdomain.com
 chown sshuser:sshuser /var/www/www.yourdomain.com
