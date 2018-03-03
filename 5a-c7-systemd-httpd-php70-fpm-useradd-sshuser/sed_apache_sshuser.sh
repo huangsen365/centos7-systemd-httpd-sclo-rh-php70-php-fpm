ab278b45ae567dc3c68f0d1781cd8275ef4b7f95
Wing Huang <huangsen365@gmail.com>
2018-03-04 01:56:02 +0800

    modified:   5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/Dockerfile 	modified:   5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/sed_apache_sshuser.sh

--- 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/sed_apache_sshuser.sh
+++ 5a-c7-systemd-httpd-php70-fpm-useradd-sshuser/sed_apache_sshuser.sh
@@ -1,4 +1,2 @@
-sed -i 's/User\ apache/User\ sshuser/g' /etc/httpd/conf/httpd_test.conf
-echo "User apache replaced to User sshuser"
-sed -i 's/Group\ apache/Group\ sshuser/g' /etc/httpd/conf/httpd_test.conf
-echo "Group apache replaced to Group sshuser"
+sed -i -e 's/User\ apache/User\ sshuser/g' -e 's/Group\ apache/Group\ sshuser/g' /etc/httpd/conf/httpd.conf
+echo "User/Group apache replaced to User/group sshuser"
